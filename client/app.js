e9ff72f943f73dd5a82a2b325dacfc790b82e523
Stephen Dowling <x21189994@student.ncirl.ie>
2024-04-04 20:05:03 +0100

    added removeFromCart function

--- client/app.js
+++ client/app.js
@@ -17,7 +17,10 @@ var call = client.addToCart(function(error, response) {
 
 })
 
-function addItemToCart(){
+
+
+
+function addToCart(){
   var name = readlineSync.question("What is the name of the item?: ")
   var price = parseFloat(readlineSync.question("How much does the item cost?: "))
 
@@ -25,72 +28,42 @@ function addItemToCart(){
     name: name,
     price: price
   })
+  call.end()
 }
 
-function finishAddingItems() {
-  call.end();
-}
-
-while (true) {
-  var option = readlineSync.keyIn('Press "a" to add an item, or "q" to finish: ', { limit: 'aq' });
-
-  if (option === 'a') {
-    addItemToCart();
-  } else if (option === 'q') {
-    finishAddingItems();
-    break;
+function removeFromCart(){
+  var name = readlineSync.question("What is the name of the item you wish to remove?: ")
+  try{
+      client.removeFromCart({
+        name:name
+      }, function(error, response){
+        try{
+          if(response.removeMsg){
+            console.log(response.removeMsg)
+          }
+          else{
+            console.log(response.removeMsg)
+          }
+        }
+        catch(e){
+          console.log("server issue")
+        }
+      })
+  }catch(e){
+    console.log("Error occured")
   }
 }
 
 
+while (true) {
+  var option = readlineSync.keyIn('Press "a" to add an item, "r" to remove an item, or "q" to finish: ', { limit: 'aqr' });
 
+  if (option === 'a') {
+    addToCart();
+  } else if(option === 'r'){
+    removeFromCart();
+  } else if (option === 'q') {
 
-
-
-
-// var call = client.totalCartValue(function(error, response) {
-//
-//   if(error){
-//     console.log("An error occured")
-//   } else{
-//     console.log("You have ordered " + response.count + " items, the total cost is: " + response.price + ". Your receipt is printed below: ")
-//     console.log(response.receipt)
-//     console.log("Thank you for shopping with us!")
-//   }
-// })
-//
-// while(true) {
-//   var item_name = readlineSync.question("What is the item called? (q to Quit): ")
-//   if(item_name.toLowerCase() === 'q') {
-//     break
-//   }
-//   var price = readlineSync.question("How much does the item cost?: ")
-//
-//   call.write({
-//     name: item_name,
-//     price: price
-//   })
-// }
-//
-// call.end()
-
-// var product_proto = grpc.loadPackageDefinition(packageDefinition).productinfo
-// var client2 = new product_proto.getProductInfo("0.0.0.0:40000", grpc.credentials.createInsecure());
-//
-// var call = client2.ProductInfo({ });
-//
-// call.on('data', function(response) {
-//   console.log("Name: "+response.name)
-//   console.log("Description: "+response.description)
-//   console.log("Price: "+response.price)
-//   console.log("In stock: "+response.stock)
-//   console.log("*****")
-// });
-//
-// call.on('end', function() {
-//
-// });
-//
-// call.on('error', function(e) {
-//   console.log(e)
-// });
+    break;
+  }
+}
--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var packageDefinition = protoLoader.loadSync(
 var retail_proto = grpc.loadPackageDefinition(packageDefinition).retail
 
 var msg = ""
+var removeMsg = ""
 var cart = [];
 var item = {
   name: "",
@@ -14,103 +15,58 @@ var item = {
 }
 
 function addToCart(call, callback) {
-  msg = "item added succesfully"
-  call.on('data', function(request) {
-    var newItem = {
-      name: request.name,
-      price: request.price
-    }
-    cart.push(newItem)
-
-    console.log("Added item: "+newItem.name)
-    console.log(cart)
-  })
-
-  call.on('end', function() {
-    callback(null, {
-      msg: msg
+  
+    call.on('data', function(request) {
+      var newItem = {
+        name: request.name,
+        price: request.price
+      }
+      cart.push(newItem)
+      msg = "item added succesfully"
+      console.log("Added item: "+newItem.name)
+      console.log(cart)
     })
-  })
 
-  call.on('error', function(e) {
-    console.log("An error occured")
-  })
+    call.on('end', function() {
+      callback(null, {
+        msg: msg
+      })
+    })
+
+    call.on('error', function(e) {
+      console.log("An error occured")
+    })
+}
+
+function removeFromCart(call, callback) {
+  // Extract the request object from the gRPC call
+  const request = call.request;
+
+  // Check if the item is in the cart
+  var found = false;
+  for (var i = 0; i < cart.length; i++) {
+    if (request.name.toLowerCase() === cart[i].name.toLowerCase()) {
+      // Remove the item from the cart
+      cart.splice(i, 1);
+      found = true;
+      console.log(cart); //print the cart to the console for clarity
+      break; // Exit the loop after removing the item
+    }
+  }
+
+  // Prepare the response message
+  var responseMsg = found ? request.name + " was removed successfully" : request.name + " was not found in the cart";
+  // Send the response back to the client
+  callback(null, {
+    removeMsg: responseMsg
+  });
 }
 
 
-// function totalCartValue(call, callback) {
-//   var count = 0
-//   var price = 0
-//   var receipt = ""
-//
-//   call.on('data', function(request) {
-//     price += request.price
-//     count += 1
-//     receipt += request.name+", €"+request.price+"\n"
-//   })
-//
-//   call.on('end', function() {
-//     callback(null, {
-//       count: count,
-//       price: price,
-//       receipt: receipt
-//     })
-//   })
-//
-//   call.on('error', function(e) {
-//     console.log("An error occured")
-//   })
-// }
 
-// var data = [
-//   {
-//     name: "Bread",
-//     description: "A loaf of fresh bread",
-//     price: 2.00,
-//     stock: true
-//   },
-//   {
-//     name: "Milk",
-//     description: "A litre of low fat milk",
-//     price: 1.50,
-//     stock: true
-//   },
-//   {
-//     name: "Fish",
-//     description: "Fresh catch of the day",
-//     price: 4.00,
-//     stock: true
-//   },
-//   {
-//     name: "Onions",
-//     description: "A net of brown onions",
-//     price: 1.00,
-//     stock: false
-//   },
-//   {
-//     name: "Peppers",
-//     description: "A packet of bell peppers",
-//     price: 1.50,
-//     stock: true
-//   }
-// ]
-//
-//
-//
-// function getProductInfo(call, callback) {
-//   for(var i = 0;i < data.length; i++){
-//     call.write({
-//       name: data[i].name,
-//       description: data[i].description,
-//       price: data[i].price,
-//       stock: data[i].stock
-//     })
-//   }
-//   call.end()
-// }
 
 var server = new grpc.Server()
-server.addService(retail_proto.Cart.service, { addToCart: addToCart })
+server.addService(retail_proto.Cart.service, { addToCart: addToCart, removeFromCart: removeFromCart })
 server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
